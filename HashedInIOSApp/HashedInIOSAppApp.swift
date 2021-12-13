b262fddc85ebf696747f06e454d30ac35bc64738
Hitesh Garg <hitegarg@deloitte.com>
2021-12-13 13:38:36 +0530

    Initial Commit

---
+++ HashedInIOSApp/HashedInIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashedInIOSAppApp.swift
+//  HashedInIOSApp
+//
+//  Created by hitegarg on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HashedInIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
